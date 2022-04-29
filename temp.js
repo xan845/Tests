d18710cff80d83fe1000b8252e02e8d03c4059aa
Paul <52319567+devnullsans@users.noreply.github.com>
2022-04-29 23:58:14 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,14 +1,17 @@
-function sumPairs(ints, s) {
-  let a, d = ints.length + 1;
-  for (i = 0; i < ints.length; i++) {
-    const ds = s - ints[i];
-    for (j = i + 1; j < ints.length; j++) {
-      const ji = j-i;
-      if ((ds == ints[j]) &&(d > ji)) {
-         a = [ints[i], ints[j]];
-         d = ji;
-      }
-    }
-  }
-  return a;
+ map = new Map([
+  ['I', 1], ['V', 5], ['X', 10],
+  ['L', 50], ['C', 100], ['D', 500],
+  ['M', 1000]
+]);
+let num = 0, pre = 0;
+for (const c of 'MMVIII') {
+  const v = map.get(c);
+  if (pre < v) {
+    num -= pre;
+    num += v - pre;
+  } else 
+    num += v;
+  pre = v;
+  console.log(c, v);
+  console.log(num, pre);
 }
