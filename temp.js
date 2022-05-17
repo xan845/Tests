61fd2b8e3c1c6b40121b58103162acfa4aac9dc2
Paul <52319567+devnullsans@users.noreply.github.com>
2022-05-17 23:46:47 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,17 +1,18 @@
- map = new Map([
-  ['I', 1], ['V', 5], ['X', 10],
-  ['L', 50], ['C', 100], ['D', 500],
-  ['M', 1000]
-]);
-let num = 0, pre = 0;
-for (const c of 'MMVIII') {
-  const v = map.get(c);
-  if (pre < v) {
-    num -= pre;
-    num += v - pre;
-  } else 
-    num += v;
-  pre = v;
-  console.log(c, v);
-  console.log(num, pre);
+function treeByLevels (root) {
+  const f = [];
+  const q = [];
+  if (root) {
+    q.push(root);
+    while (q.length) {
+      magic(q, f);
+    }
+  }
+  return f;
+}
+
+function magic(q, f) {
+  const node = q.shift();
+  f.push(node.value);
+  if (node.left) q.push(node.left);
+  if (node.right) q.push(node.right);
 }
